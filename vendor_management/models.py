d3591b886e98852f3198807bbf7f3cf8d9f272ff
samhithaarani <n180441@rguktn.ac.in>
2024-05-10 00:13:42 +0530

    fixing serializers.py

--- vendor_management/models.py
+++ vendor_management/models.py
@@ -14,6 +14,7 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.name
+
 class PurchaseOrder(models.Model):
     po_number = models.CharField(max_length=100, unique=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
--- vendor_management/serializers.py
+++ vendor_management/serializers.py
@@ -8,6 +8,13 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrder
         fields = '__all__'
+        # Specify datetime format for fields
+        extra_kwargs = {
+            'order_date': {'format': '%Y-%m-%dT%H:%M:%S'},
+            'delivery_date': {'format': '%Y-%m-%dT%H:%M:%S'},
+            'issue_date': {'format': '%Y-%m-%dT%H:%M:%S'},
+            'acknowledgment_date': {'format': '%Y-%m-%dT%H:%M:%S'},
+        }
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
